200c3a532cab30f53a1296b939516344073a404d
Sergio Freire <sergio.freire@gmail.com>
2019-08-30 15:55:50 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,10 @@
 
 sh -c "echo $*"
 
+sh -c "NAME: $INPUT_NAME"
+sh -c "NAME: $INPUT_fILE"
+
 sh -c "cat /README.md"
 sh -c "cat /github/workspace/README.md"
 sh -c "cat /github/workspace/target/surefire-reports/TEST-com.xpand.java.CalcTest.xml"
+sh -c "find /github/workspace/ -type f"
