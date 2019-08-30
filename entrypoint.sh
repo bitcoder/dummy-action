ba0f39e70b0f9fa6ae431cba9e60052cae8e788b
Sergio Freire <sergio.freire@gmail.com>
2019-08-30 17:10:23 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,3 +10,6 @@ sh -c "cat /github/workspace/README.md"
 #sh -c "cat /github/workspace/target/surefire-reports/TEST-com.xpand.java.CalcTest.xml"
 sh -c "cat /github/workspace/$INPUT_FILE"
 sh -c "find /github/workspace/ -type f"
+
+
+curl -H "Content-Type: multipart/form-data" -u $INPUT_JIRA_USERNAME:$INPUT_JIRA_PASSWORD -F "file=@/github/workspace/$INPUT_FILE" "$INPUT_JIRA_URL/rest/raven/1.0/import/execution/junit?projectKey=$INPUT_PROJECT_KEY"
